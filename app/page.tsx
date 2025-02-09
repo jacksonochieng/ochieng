87dccd195b505873a8e0d5f82fbde43790f98112
jacksonochieng <136727294+jacksonochieng@users.noreply.github.com>
2025-02-09 14:52:56 +0300

    updated password information

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ const RegistrationForm = () => {
           <input
             type="password"
             id="password"
-            name="password"
+            name="enter password"
             value={formData.password}
             onChange={handleChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
